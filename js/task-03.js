8780a4986f39ce615b2b7786af9c844eb1ecefbf
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-03 00:43:06 +0200

    Задание 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,14 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const galleryRef = document.querySelector('.gallery');
+console.log(galleryRef);
+
+const galleryItems = images
+  .map(({ url, alt }) => 
+  `<li class="gallery__item"> <img src="${url}" alt="${alt}" /> </li>`)
+  .join('');
+
+galleryRef.insertAdjacentHTML("beforeend", galleryItems);
