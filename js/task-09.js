4d1e9d5c8977514fb43199c4626c509451ba0242
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-05 01:04:35 +0200

    Задание 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanColorRef = document.querySelector('.color');
+const buttonRef = document.querySelector('.change-color');
+
+buttonRef.addEventListener('click', changeColor);
+
+function changeColor() { 
+  const colorRandom = getRandomHexColor();
+
+  document.body.style.backgroundColor = colorRandom;
+
+  spanColorRef.textContent = colorRandom;
+};
