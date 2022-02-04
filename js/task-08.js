0364f639ac7a9ea7fb83a7a12ba4d71da93d3a70
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-05 00:32:27 +0200

    Задание 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form');
+// console.log(formRef);
 
+formRef.addEventListener('submit', formSubmit)
+
+
+function formSubmit (event) { 
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    if (!email.value || !password.value) { 
+        alert ('! Пожалуйста заполните все поля (P.S. Я не шучу)');
+    } else { 
+        const formReply = { email: email.value, password: password.value };
+        console.log(formReply);
+        event.currentTarget.reset();
+    };
+};
