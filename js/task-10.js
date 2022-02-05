416963da4be96487b7c0c0b968f03bf711a54f67
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-02-05 20:43:12 +0200

    Задание 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesRef = document.querySelector('#boxes');
+const createRef = document.querySelector('[data-create]');
+const destroyRef = document.querySelector('[data-destroy]');
+const inputRef = document.querySelector('#controls').firstElementChild;
+
+createRef.addEventListener('click', takeInput);
+
+function takeInput() {
+  const amount = inputRef.value;
+  createBoxes(amount);
+};
+
+function createBoxes(amount) { 
+  const firstBox = 30;
+  const nextBoxes = [];
+
+  for (let i = 0; i < amount; i += 1) { 
+    const divEl = document.createElement('div');
+    divEl.style.width = firstBox + i * 10 + 'px';
+    divEl.style.height = firstBox + i * 10 + 'px';
+    divEl.style.backgroundColor = getRandomHexColor();
+    nextBoxes.push(divEl);
+  };
+  boxesRef.append(...nextBoxes);
+};
+
+destroyRef.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() { 
+  boxesRef.innerHTML = '';
+};
